98d5f9d07577188bfe2397e9a3158d538c46f47b
Ha1fByte <natalievcardinali@gmail.com>
2017-03-31 01:22:11 -0400

    Add files via upload
    SlackOFF currently uses a slightly modified version of the bot provided by this tutorial and has been running headless on a raspberry pi 3
    
    https://www.fullstackpython.com/blog/build-first-slack-bot-python.html

---
+++ SlackOff.py
@@ -0,0 +1,77 @@
+import os
+import time
+from slackclient import SlackClient
+import json
+
+# starterbot's ID as an environment variable
+BOT_ID = os.environ.get("BOT_ID")
+
+# constants
+AT_BOT = "<@" + BOT_ID + ">"
+EXAMPLE_COMMAND = "do"
+COMMAND_1 = "channel name"
+COMMAND_2 = "channel users"
+
+# instantiate Slack & Twilio clients
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+
+def list_channels():
+    channels_call = slack_client.api_call("channels.list")
+    if channels_call.get('ok'):
+        return channels_call['channels']
+    return None
+
+
+def handle_command(command, channel):
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+    """
+    #response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
+    #          "* command with numbers, delimited by spaces."
+    var = slack_client.api_call("channels.info", channel=channel)
+    response = " "
+    if command.startswith(EXAMPLE_COMMAND):
+        response = "Sure...write some more code then I can do that!"
+    elif command.startswith(COMMAND_1):
+        response = "We are currently in : \"" + var["channel"]["name"] +"\""
+    elif command.startswith(COMMAND_2):
+        response = "There are: " + str(len(var["channel"]["members"])) + " in this channel. \n" \
+                                                                    "User Names: "
+        var2 = slack_client.api_call("users")
+        for i in range(len(var["channel"]["members"])):
+            response +=var["channel"]["members"][1] + "\n"
+
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+
+
+def parse_slack_output(slack_rtm_output):
+    """
+        The Slack Real Time Messaging API is an events firehose.
+        this parsing function returns None unless a message is
+        directed at the Bot, based on its ID.
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                       output['channel']
+    return None, None
+
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("StarterBot connected and running!")
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
